3b2487fac2457ac7a69f81dabf4a36cc43337af4
Rohit Jaiswal <rohitjaiswal1101.com>
2026-08-06 11:13:52 +0530

    name change

--- lib/main.dart
+++ lib/main.dart
@@ -63,7 +63,7 @@ class _MyAppState extends State<MyApp> {
       ],
       child: MaterialApp(
         navigatorKey: _navigatorKey,
-        title: 'Globe Link Driver',
+        title: 'GlobeLink Driver',
         debugShowCheckedModeBanner: false,
         theme: AppTheme.lightTheme,
         home: BlocConsumer<AuthBloc, AuthState>(
